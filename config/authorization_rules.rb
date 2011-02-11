9e28bf0ee749196f1f350df2395b68b6eff12999
Ernst <ernst@hisplace.net>
2011-02-12 02:11:42 +1100

    provide rules for first simple declarative_authorization tests

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,7 +1,21 @@
 authorization do
-  role :admin do
-    has_permission_on :locations, :to => [:index]
+
+  role :world_config do
+    has_permission_on [:locations, :venues, :areas, :roles, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
   end
+
+  role :superuser do
+    has_permission_on [:locations, :venues, :areas, :roles, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+  end
+
+  role :admin do
+    has_permission_on [:locations, :venues, :areas, :roles, :users], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+  end
+
+  role :registrar do
+    has_permission_on [:locations, :venues, :areas, :roles, :users], :to => [:index, :show]
+  end
+
 end
 
 # index show new create edit update destroy
